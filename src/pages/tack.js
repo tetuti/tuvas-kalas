e5651599436803a07f65df676f1ec56e493e026f
tetuti <larsarle@gmail.com>
2019-05-14 00:03:55 +0200

    fix tack

--- src/pages/tack.js
+++ src/pages/tack.js
@@ -9,7 +9,16 @@ const TackPage = () => (
     <div className = 'container '>
 		<h2>Vad kul</h2>
 		<p>
-		Då syns vi på x den y!
+			Då syns vi på <em>1 Juni</em> på <a target = '_blank' rel="noopener noreferrer" href = "https://www.google.com/maps/place/Konstrukt%C3%B6rsgatan+70,+587+37+Link%C3%B6ping">Konstruktörsgatan 70</a>
+		</p>
+		<p>
+			Om ni har några frågor kan ni nå Tuvas mamma eller pappa på mobilnummer: 
+		</p>
+		<p>
+		<code><em>0722 291 577</em></code> (Lars)
+		</p>
+		<p>
+		<code><em>0737 320 760</em></code> (Anna)
 		</p>
 		
 	</div>
